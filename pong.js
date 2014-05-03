1a06e978f7d185acc4e141f1883c7f8f9a42d22f
Ridlo W. Wibowo <ridlo.w.wibowo@gmail.com>
2014-05-03 18:51:22 +0700

    Update pong.js

--- pong.js
+++ pong.js
@@ -72,7 +72,7 @@ var computer = new Computer();
 var ball = new Ball(200, 300);
 
 var render = function() {
-  context.fillStyle = "#FF00FF";
+  context.fillStyle = "#FFFFFF";
   context.fillRect(0, 0, width, height);
   player.render();
   computer.render();
