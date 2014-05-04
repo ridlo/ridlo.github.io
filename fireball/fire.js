3d45163322e92bfbc5d8c5e52ae693a8f9780cd6
Ridlo W. Wibowo <ridlo.w.wibowo@gmail.com>
2014-05-04 15:57:20 +0700

    Update fire.js

--- fireball/fire.js
+++ fireball/fire.js
@@ -233,10 +233,10 @@ Computer.prototype.update = function(ball) {
     var x_pos = ball.x;
     var diff = -((this.stick.x + (this.stick.width / 2)) - x_pos);
     
-    if (diff < 0 && diff < -4) { // max speed left
-        diff = -5;
-    } else if(diff > 0 && diff > 4) { // max speed right
-        diff = 5;
+    if (diff < 0 && diff < -6) { // max speed left
+        diff = -8;
+    } else if(diff > 0 && diff > 6) { // max speed right
+        diff = 8;
     }
     this.stick.move(diff, 0);
 };
