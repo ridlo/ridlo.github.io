8927c730ad1e51ab4e560d773ec44032015eac57
ridlo <ridlo.w.wibowo@gmail.com>
2015-05-27 09:29:55 +0700

    change avatar's name

--- islamic/static/js/QiblaFinderEmbed.js
+++ islamic/static/js/QiblaFinderEmbed.js
@@ -76,7 +76,7 @@ function initialize(){
     qiblaMap.fitBounds(defaultBounds);
 
 
-    var contentString = '<strong>Hello</strong>, this is Paimin!<br>Have you prayed?';
+    var contentString = '<strong>Hello</strong>, this is Mas Jo!<br>Have you prayed?';
 
     var infowindow = new google.maps.InfoWindow({
         content: contentString
