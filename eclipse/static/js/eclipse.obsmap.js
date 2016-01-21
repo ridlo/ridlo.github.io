9071359f019a10cbdccf9d400818d06d377cfe69
ridlo <ridlo.w.wibowo@gmail.com>
2016-01-21 19:58:11 +0700

    test

--- eclipse/static/js/eclipse.obsmap.js
+++ eclipse/static/js/eclipse.obsmap.js
@@ -9,13 +9,18 @@ function initMap() {
     center: uluru
   });
 
-  var contentString = 
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<div id="bodyContent">'+
       '<p><b>Palangkaraya</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
       'sandstone rock formation in the southern part of the '+
       'Northern Territory, central Australia. It lies 335 km (208 mi)</p>'+
       '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
       'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>';
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
 
   var infowindow = new google.maps.InfoWindow({
     content: contentString,
