7abbe8b2dabb4d42937eb3d49f7f5a8ca44f07bf
ridlo <ridlo.w.wibowo@gmail.com>
2016-01-21 19:52:46 +0700

    test

--- eclipse/static/js/eclipse.obsmap.js
+++ eclipse/static/js/eclipse.obsmap.js
@@ -3,32 +3,19 @@
 // The maximum width of the info window is set to 200 pixels.
 
 function initMap() {
-  var uluru = {lat: -2.211931, lng: 113.907327};
+  var  = {lat: -2.760853, lng: 117.173719};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 3,
     center: uluru
   });
 
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+  var contentString = 
+      '<p><b>Palangkaraya</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
       'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
+      'Northern Territory, central Australia. It lies 335 km (208 mi)</p>'+
       '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
       'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
+      '(last visited June 22, 2009).</p>';
 
   var infowindow = new google.maps.InfoWindow({
     content: contentString,
@@ -38,7 +25,7 @@ function initMap() {
   var marker = new google.maps.Marker({
     position: uluru,
     map: map,
-    title: 'Uluru (Ayers Rock)'
+    title: 'Palangkaraya'
   });
 
   marker.addListener('click', function() {
