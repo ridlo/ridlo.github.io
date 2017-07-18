d5cc2dbbbe865cbbae1e5206d7fcf339186fef30
ridlo <ridlo.w.wibowo@gmail.com>
2017-07-18 00:29:46 -0400

    aneh

--- islamic/static/js/view.js
+++ islamic/static/js/view.js
@@ -52,19 +52,19 @@
 
 //Kaaba Time
 
-var date = new Date();
-var lat = 50;
-var lng = -70.5886011;
-var sunpos = SunCalc.kiblaTimes(date, lat, lng, 120);
-console.log("yaya: ", sunpos);
+// var date = new Date();
+// var lat = 50;
+// var lng = -70.5886011;
+// var sunpos = SunCalc.kiblaTimes(date, lat, lng, 120);
+// console.log("yaya: ", sunpos);
 // document.getElementById("indir").innerHTML = converttime2(sunpos['indir']);
 // document.getElementById("outdir").innerHTML = converttime2(sunpos['outdir']);
 // document.getElementById("rightangle1").innerHTML = converttime2(sunpos['rightangle1']);
 // document.getElementById("rightangle2").innerHTML = converttime2(sunpos['rightangle2']);
 
 
-var date = new Date();
-var lat = -7;
-var lng = -70.5886011;
-var sunpos = SunCalc.kiblaTimes(date, lat, lng, 65);
-console.log("mama: ", sunpos);
+// var date = new Date();
+// var lat = -7;
+// var lng = -70.5886011;
+// var sunpos = SunCalc.kiblaTimes(date, lat, lng, 65);
+// console.log("mama: ", sunpos);
